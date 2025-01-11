cc94ec63fa620eb9a50b15cbc725c338961bb462
sharan560 <sharannagarajan06@gmail.com>
2025-01-11 12:39:39 +0530

    day6

---
+++ day6/apple-project/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    //JavaScript Code to be Written Here
+  return (
+    //html code to be written here
+    <div>Header
+        <h1>Hello World</h1>
+        
+    </div>
+    
+
+  )
+}
+
+export default Header
